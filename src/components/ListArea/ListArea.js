3936b24f29c9e31d92f53515354f7cd849e0335a
ronchitech <ronchibusiness@gmail.com>
2021-01-17 22:42:35 +0800

    successfully added navlink functionalities

--- src/components/ListArea/ListArea.js
+++ src/components/ListArea/ListArea.js
@@ -1,22 +1,114 @@
-import React, { Suspense } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { Route } from 'react-router-dom';
 import classes from './ListArea.module.css';
 import Navigation from '../../Navigations/Navigation';
+import { connect } from 'react-redux';
+import * as actionTypes from '../../store/actions/index';
+const ListArea = React.memo((props) => {
+  const { onFetchTodo, mytodos, onDeleteTodo, onIsDone } = props;
+  useEffect(() => {
+    onFetchTodo();
+  }, [onFetchTodo]);
 
-const ListAll = React.lazy(() => import('./ListAll'));
-const ListDone = React.lazy(() => import('./ListDone'));
-const ListArea = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className={classes.ListArea}>
-        <Navigation />
-       
-          <Route exact path="/" component={()=> <ListAll/>} />
-          <Route  path="/done" component={ListDone} />
-     
-      </div>
-      </Suspense>
+  const deleteHandler = useCallback(
+    (id) => {
+      onDeleteTodo(id);
+    },
+    [onDeleteTodo]
   );
+  const isDoneHandler = (id, isDone) => {
+    // console.log(id);
+    onIsDone(id, isDone);
+  };
+  // console.log('mytodos', mytodos);
+  const listAll = mytodos.map((key) => {
+    // console.log(key);
+    return (
+      <span key={key.id}>
+        <li
+          style={{
+            textDecoration: key.objTodo.isDone ? 'line-through' : null,
+            background: key.objTodo.isDone ? '#DADBE6' : null,
+            color: key.objTodo.isDone ? '#2A2D34' : null,
+          }}
+          className={classes.Todolist}
+          onClick={() => isDoneHandler(key.id, !key.objTodo.isDone)}
+        >
+          {key.objTodo.todo}
+        </li>
+        <button onClick={() => deleteHandler(key.id)}>x</button>
+      </span>
+    );
+  });
+
+  const listTodo = mytodos
+    .filter((todo) => todo.objTodo.isDone !== true)
+    .map((key) => {
+      return (
+        <span key={key.id}>
+          <li
+            style={{
+              textDecoration: key.objTodo.isDone ? 'line-through' : null,
+              background: key.objTodo.isDone ? '#DADBE6' : null,
+              color: key.objTodo.isDone ? '#2A2D34' : null,
+            }}
+            className={classes.Todolist}
+            onClick={() => isDoneHandler(key.id, !key.objTodo.isDone)}
+          >
+            {key.objTodo.todo}
+          </li>
+          <button onClick={() => deleteHandler(key.id)}>x</button>
+        </span>
+      );
+    });
+  const listDone = mytodos
+    .filter((todo) => todo.objTodo.isDone !== false)
+    .map((key) => {
+      return (
+        <span key={key.id}>
+          <li
+            style={{
+              textDecoration: key.objTodo.isDone ? 'line-through' : null,
+              background: key.objTodo.isDone ? '#DADBE6' : null,
+              color: key.objTodo.isDone ? '#2A2D34' : null,
+            }}
+            className={classes.Todolist}
+            onClick={() => isDoneHandler(key.id, !key.objTodo.isDone)}
+          >
+            {key.objTodo.todo}
+          </li>
+          <button onClick={() => deleteHandler(key.id)}>x</button>
+        </span>
+      );
+    });
+
+  // console.log('remaining: ',listTodo);
+  return (
+    <div className={classes.ListArea}>
+      <Navigation />
+      <ul className={classes.List}>
+        <Route exact path="/" render={() => listAll} />
+        <Route path="/todos" render={() => listTodo} />
+        <Route path="/done" render={() => listDone} />
+      </ul>
+    </div>
+  );
+});
+
+const mapStateToProps = (state) => {
+  return {
+    mytodos: state.mytodos,
+    isLoading: state.isLoading,
+    // isDone: state.mytodos.isDone,
+    // id: state.mytodos
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchTodo: () => dispatch(actionTypes.fetchTodoStart()),
+    onDeleteTodo: (id) => dispatch(actionTypes.deleteTodoStart(id)),
+    onIsDone: (id, isDone) => dispatch(actionTypes.isDoneStart(id, isDone)),
+  };
 };
 
-export default React.memo(ListArea);
+export default connect(mapStateToProps, mapDispatchToProps)(ListArea);
